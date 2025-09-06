c486d9bd0d389d69197366b077bc65b891883147
Kedar-08 <kedarnath.kini@estuate.com>
2025-09-06 16:30:50 +0530

    Initial commit of the project files

---
+++ practice.js
@@ -0,0 +1,23 @@
+const greet  = (name) => {
+    console.log(`Hello, ${name}!`);
+};
+
+greet("Alice");
+
+
+let welcome = () => {
+    console.log("Welcome to the practice session!");
+}
+
+welcome();
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Count:", i);
+}
+
+
+let i = 1;
+while (i <= 5) {
+  console.log("Count:", i);
+  i++;
+}
